e72af8cfe192a84fef96ef65990f895eb65bfc4e
YZaitova <zaityana24@gmail.com>
2020-09-16 14:53:32 +0700

    Правит watcher

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html", gulp.series("html")).on("change", sync.reload);
+  gulp.watch("source/*.html").on("change", sync.reload);
 }
 
 exports.default = gulp.series(
